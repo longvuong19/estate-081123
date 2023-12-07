c0442f2bd5abed84da64bfa2d1a950f3f6efa05a
LongVuong <longvuong1920@gmail.com>
2023-12-07 23:34:24 +0700

    add delete and edit listing functions

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,6 +28,8 @@ const Profile = () => {
   const [filePercentage, setFilePercentage] = useState(0);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   // console.log(formData);
   // console.log(fileUploadError);
   const navigate = useNavigate();
@@ -125,6 +127,40 @@ const Profile = () => {
     }
   };
 
+  const handleShowListing = async () => {
+    try {
+      setShowListingError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((preview) =>
+        preview.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -209,6 +245,51 @@ const Profile = () => {
       <p className="text-green-700 text-xl mt-5 text-center">
         {updateSuccess ? "Your information has been updated!" : ""}
       </p>
+
+      <button className="text-green-700 w-full" onClick={handleShowListing}>
+        Show your Listing
+      </button>
+      <p className="text-red-700 mt-5 text-center">
+        {showListingError ? "Error showing your listings." : ""}
+      </p>
+
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center mt-4 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
+              <Link to={`/listings/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain cursor-pointer"
+                />
+              </Link>
+              <Link
+                to={`/listings/${listing._id}`}
+                className="flex-1 text-slate-700 font-semibold hover:underline cursor-pointer truncate"
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col items-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <button className="text-green-700 uppercase">Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/Listing.model.js";
 import User from "../models/User.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
@@ -39,6 +40,23 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie("accessToken");
     res.status(200).json("User has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  try {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, "Your can only view your own listings."));
+    }
   } catch (error) {
     next(error);
   }
