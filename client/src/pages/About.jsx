e56c865165765855af78abd454ee0b19172068eb
LongVuong <longvuong1920@gmail.com>
2024-01-09 01:00:56 +0700

    complete about and home page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About CreamCheeseEstate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Nam vel leo feugiat, elementum dui ac, condimentum tellus. Duis tempor
+        scelerisque tristique. Curabitur a ultrices urna, eget efficitur ligula.
+        Mauris nec sagittis nulla, nec finibus ex. Sed sem metus, accumsan in
+        consequat id, gravida eu nisl. Curabitur fringilla quis mi vitae mattis.
+        Nullam tristique turpis vitae massa fringilla dapibus. Nullam nec tellus
+        at nulla ultricies consectetur non a turpis. Proin eu lectus
+        ullamcorper, rhoncus eros non, accumsan turpis. Sed ultrices ornare
+        volutpat. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Suspendisse erat libero, mattis ac neque
+        sed, sagittis malesuada erat.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Nulla commodo elit at dui ultricies, at commodo diam egestas. Aliquam
+        facilisis fringilla magna vel tincidunt. Nam eu imperdiet ante, sit amet
+        auctor nunc. Nullam laoreet nisl sed arcu aliquet, a hendrerit nisi
+        volutpat. Donec sed odio vulputate, pulvinar erat et, laoreet urna. Nunc
+        fermentum, est at consequat euismod, justo tellus faucibus odio, nec
+        hendrerit metus urna at erat. Etiam non felis ipsum. Praesent efficitur
+        auctor nulla et pharetra. Nunc bibendum est ipsum, efficitur euismod
+        justo varius ut. Mauris condimentum at tellus a aliquam. Etiam pulvinar
+        elit vitae elementum convallis.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Praesent sit amet consequat felis, ac convallis enim. Proin ullamcorper
+        turpis vitae iaculis semper. Mauris laoreet purus et tempor tristique.
+        Mauris vel placerat nisl. Integer non ullamcorper libero, ut tincidunt
+        libero. In venenatis, magna sit amet condimentum pulvinar, orci dolor
+        suscipit est, ac finibus leo diam vehicula lectus. In viverra mi at
+        turpis hendrerit mollis. Fusce urna orci, venenatis nec dignissim vitae,
+        consectetur id arcu. Suspendisse elementum varius neque, at pulvinar est
+        aliquam vel.
+      </p>
+    </div>
+  );
 };
 
 export default About;
